2c066beddc7a288190202a0d4c5520074a39e449
Ubuntu <sak123@Ubuntu16.xtym4keuewxepby35kf0zuepyg.gx.internal.cloudapp.net>
2020-04-03 22:56:08 -0500

    adds a python(py) file

---
+++ python/main.py
@@ -0,0 +1,16 @@
+##### SWAP
+#Shelby Kawejsza 
+
+##### Main Program
+a = (input('Give me an interger number ->A:'))#ask for a
+z = (input('Give me an interger number ->Z:'))#ask for z
+
+
+#swap them
+temp = a
+a = z
+z = temp
+print('A is now:', a)
+print('Z is now:', z)
+
+
