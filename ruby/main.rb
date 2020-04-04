1858c281c6765fcec0ea1c8788828fa06729d02a
Ubuntu <sak123@Ubuntu16.xtym4keuewxepby35kf0zuepyg.gx.internal.cloudapp.net>
2020-04-03 23:03:32 -0500

    adds a ruby file

---
+++ ruby/main.rb
@@ -0,0 +1,10 @@
+def find_missing(sequence)
+  consecutive     = sequence.each_cons(2)
+  differences     = consecutive.map { |a,b| b - a }
+  sequence        = differences.max_by { |n| differences.count(n) }
+  missing_between = consecutive.find { |a,b| (b - a) != sequence }
+  missing_between.first + sequence
+end
+
+print find_missing([2,4,6,10])
+
